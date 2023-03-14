06cd937cf7c1ca473f4be09ed9bf9dfd53d12f3d
Jason den Dulk <jasondendulk@catalyst-au.net>
2023-03-14 15:01:10 +1100

    Initial Commit.

---
+++ define.class.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Custom 'conditional text' field definition
+ *
+ * @package   profilefield_conditionaltext
+ * @author    Jason den Dulk <jasondendulk@catalyst-au.net>
+ * @copyright 2023, Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class profile_define_conditionaltext extends profile_define_base {
+    function define_save_preprocess($data) {
+        $data->defaultdata = NULL;
+        return $data;
+    }
+}
